e3c49a7c35fcc5d0c642290b04a4110b87f86ff8
cindysoares <cindy.as@gmail.com>
2017-10-11 00:22:21 -0300

    configuring an embedded database

--- src/main/java/br/com/muxi/equipments/EquipmentsApplication.java
+++ src/main/java/br/com/muxi/equipments/EquipmentsApplication.java
@@ -2,8 +2,10 @@ package br.com.muxi.equipments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class EquipmentsApplication {
 
 	public static void main(String[] args) {
