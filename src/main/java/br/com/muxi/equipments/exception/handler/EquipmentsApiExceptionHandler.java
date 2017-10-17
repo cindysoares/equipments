35959b69488c329de36a8eda08a59860097863ba
cindysoares <cindy.as@gmail.com>
2017-10-17 16:07:05 -0200

    returning constraints violations messages as a json

--- src/main/java/br/com/muxi/equipments/exception/handler/EquipmentsApiExceptionHandler.java
+++ src/main/java/br/com/muxi/equipments/exception/handler/EquipmentsApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package br.com.muxi.equipments.exception.handler;
 
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,5 +24,15 @@ public class EquipmentsApiExceptionHandler {
 	public ResponseEntity<Object> handleValidationException(final MethodArgumentNotValidException ex, WebRequest request) {
 		return new ResponseEntity<Object>(new InvalidResourceError(ex), HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler({RollbackException.class})
+	public ResponseEntity<Object> handleValidationException(final RollbackException ex, WebRequest request) {
+		Throwable cause = ex.getCause();
+		if (cause instanceof ConstraintViolationException) {
+			return new ResponseEntity<Object>(new InvalidResourceError((ConstraintViolationException) cause), HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<Object>(new EquipmentsApiError(ex), HttpStatus.BAD_REQUEST);
+		}
+	} 
 	
 }
--- src/main/java/br/com/muxi/equipments/exception/handler/InvalidResourceError.java
+++ src/main/java/br/com/muxi/equipments/exception/handler/InvalidResourceError.java
@@ -3,15 +3,27 @@ package br.com.muxi.equipments.exception.handler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 public class InvalidResourceError extends EquipmentsApiError {
 	
+	private static final String INVALID_ARGUMENTS = "Invalid arguments.";
+	
 	protected List<InvalidFieldValueError> fieldErrors;
+	
+	public InvalidResourceError(ConstraintViolationException ex) {
+		super(ex);
+		this.message = INVALID_ARGUMENTS;
+		this.fieldErrors = ex.getConstraintViolations().stream()
+				.map(violation -> new InvalidFieldValueError(violation.getPropertyPath().toString(), violation.getMessage()))
+				.collect(Collectors.toList());
+	}
 
 	public InvalidResourceError(MethodArgumentNotValidException ex) {
 		super(ex);
-		this.message = "Invalid arguments.";
+		this.message = INVALID_ARGUMENTS;
 		this.fieldErrors = ex.getBindingResult().getFieldErrors().stream()
 				.map(fieldError -> new InvalidFieldValueError(fieldError.getField(), fieldError.getDefaultMessage()))
 				.collect(Collectors.toList());
